301ad3eb8b67f13dacc71ea24d40f11eb836d053
AlanSuarez98 <alansuarez641otmail.com>
2022-11-07 16:17:22 -0300

    modificacion html

--- SnakeGame/Juego/form.php
+++ SnakeGame/Juego/form.php
@@ -5,7 +5,6 @@ $message = $_POST['message'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
